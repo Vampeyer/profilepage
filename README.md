9e94f9854a7feba1aa8f760e452bee779ccbc2d1
Vampeyer <jnowtxtnownow@gmail.com>
2022-07-24 20:37:44 -0400

    message

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# profilepage
-Profile Page 
+# profilesite
+
+
+Ladies, Gentleman, and bypassers alike ,
+
+Welcome ,  
+to my landing page ,    I hope you  find yourself comfortable and enjoy my code as well as the page. 
+this page is in infancy at this point but however will be continued upon until animations , etc and all will show the full diaplay in the future
+
+the designs are quite fun to play with though , theres no telling 
+what the final product will be like !  
+
+Please enjoy , and feel free to judge and participate alike! 
+
+
+
+------------------------- 
+- - IMPORTANT  NOTES - -    
+* Javascript on lines 1 and lines 3 must be commented - in for activation at this time.
+
+
+
+*************/  
+
+Page still requires 
+
+- Burger 
+
+- Some more java with a variable / function 
+
+*/ ****************
+
+
+
+
+
+
+
+
+------------------------------------------
+
+* about me > img and contact gif > img have been "ultra" customized for size , use as needed. 
---
+++ js/app.js
@@ -0,0 +1,55 @@
+// hamburger menus are a learning experience.   they didnt show us anything on that , - bugt hopefully I will be able to provide 
+// a solution ! 
+
+
+let btn = document.querySelector('button')
+let list = document.querySelector('.list')
+
+btn.addEventListener('click', () => {
+   if(list.style.display === 'none' ){
+      list.style.display = 'block' ;
+   }else{
+    list.style.display = 'none' ; 
+   }
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Warning , you are entering the coolest  website ever");
+ // confirm("If you wish to continue press Okay. ");
+
+// NOTHING HERE YET.
+
+
+
+//const hamburger = document.querySelector(".hamburger");
+//const navMenu = document.querySelector(".hamburger");
+
+//hamburger.addEventListener("click" , () => {
+  //  hamburger.classList.toggle("active");
+  //  hamburger.classList.toggle("active");
+
+//})             
+
+
+
+
+
+
+
+
+
+
+//====================================================
+let student = "student has passed"
+
+ console.log(student)
